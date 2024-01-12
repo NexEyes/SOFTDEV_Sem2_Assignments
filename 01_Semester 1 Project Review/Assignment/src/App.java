ad95bc481f4b3138dc43230dabd4b372a3e21c8b
Keith Shovan <shovakei000@warren.k12.in.us>
2024-01-12 10:55:59 -0500

    updates

--- 01_Semester 1 Project Review/Assignment/src/App.java
+++ 01_Semester 1 Project Review/Assignment/src/App.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int total;
+
+
+        
+
+        try(Scanner nums = new Scanner(System.in)){
+
+            System.out.println("Enter the numerator");
+            int firstNum= nums.nextInt();
+            System.out.println("Enter the denominator");
+            int secondNum= nums.nextInt();
+
+            total = firstNum/secondNum;
+
+            System.out.println(total);
+        }catch(Exception ex){
+            System.out.println("Undefined");
+
+        }
+
     }
 }
