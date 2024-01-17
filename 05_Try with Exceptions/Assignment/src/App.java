f2e712206008719711dbca64274e6f12fabeea8b
Keith Shovan <shovakei000@warren.k12.in.us>
2024-01-17 10:19:35 -0500

    col

--- 05_Try with Exceptions/Assignment/src/App.java
+++ 05_Try with Exceptions/Assignment/src/App.java
@@ -1,23 +1,5 @@
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        int total;    
-
-        try(Scanner nums = new Scanner(System.in)){
-
-            System.out.println("Enter the numerator");
-            int firstNum= nums.nextInt();
-            System.out.println("Enter the denominator");
-            int secondNum= nums.nextInt();
-
-            total = firstNum/secondNum;
-
-            System.out.println(total);
-        }catch(Exception ex){
-            System.out.println("Undefined");
-
-        }
 
     }
 }
