8826b10d40316cf3bd3101b945479590a56249c4
Keith Shovan <shovakei000@warren.k12.in.us>
2024-02-07 11:01:30 -0500

    we

--- 03_File Handling/03_Creating and Writing a File/Assignment/src/App.java
+++ 03_File Handling/03_Creating and Writing a File/Assignment/src/App.java
@@ -1,5 +1,28 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String filename = "src/start.text";
+        String newFileName = "src/ender.txt";
+
+        Path filepath = Paths.get(filename);
+        List<String> lines = Files.readAllLines(filepath);
+
+                FileWriter fileWriter = new FileWriter(newFileName, true);
+        BufferedWriter writer = new BufferedWriter(fileWriter);
+
+        for(String line: lines) {
+        writer.append(line);
+        writer.append("\n");
+        }   
+        
+
+
+        writer.close();
     }
 }
