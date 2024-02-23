5a66a1aeecf173a16d54264f8e7df8b5f69db767
Keith Shovan <shovakei000@warren.k12.in.us>
2024-02-23 10:55:58 -0500

    calm

--- 04_Functional Programming_Streams/05_Iterating Streams/Assignment/src/App.java
+++ 04_Functional Programming_Streams/05_Iterating Streams/Assignment/src/App.java
@@ -5,7 +5,7 @@ import java.util.stream.IntStream;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // use streams to generate a list of numbers from 0 to 10
+ 
         // use streams and Functions with lambdas to make a list with 5 added to each numbers in the ints list
         // use streams and foreach to print out the values
 
@@ -15,16 +15,14 @@ public class App {
         //TODO:  use foreach to print intsPlus5Div2
 
         List<Integer> ints = IntStream.rangeClosed(0, 10).boxed().toList();
-        Function<Integer, Integer> add5Func = (number) -> number;  //TODO:  acutally implement the lambda correctly
-
+        Function<Integer, Integer> add5Func = (number) -> number + 5;    
         List<Integer> intsPlus5 = ints.stream().map(add5Func).toList();
-
         Consumer<Integer> intPrinter = (number) -> System.out.println(number);
         intsPlus5.stream().forEach(intPrinter);
-
-        //TODO:  write a div2Func Function like I did with add5Func
-        //TODO:  apply div2Func to intsPlus5 with map
-        //TODO:  print with foreach.  
-
+        
+        Function<Integer, Integer> div2Func = (number) -> number / 2;
+        List<Integer> divAndPlus = intsPlus5.stream().map(div2Func).toList();
+        divAndPlus.stream().forEach(intPrinter);
+        
     }
 }
